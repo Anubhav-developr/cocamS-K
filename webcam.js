21d1c93e5a872112e86ffc0dd7734ac7e525ae0d
Anubhav-developr <anubhavm1234@gmail.com>
2021-12-09 22:00:09 +0530

    added more recording features

--- webcam.js
+++ webcam.js
@@ -1,7 +1,93 @@
+//screen recording ke liye
+// const recordScreen = async function() {
+// //  window.alert("hello")
+// const stream = await navigator.mediaDevices.getDisplayMedia({
+//   video : {
+//     mediaSource : "screen"
+//   }
+// })
+// const data = [];
+// const mediaRecorder = MediaRecorder(stream);
+// mediaRecorder.ondataavailable = (e) =>{
+//   data.push(e.data);
+// }
+// mediaRecorder.start();
+// mediaRecorder.onstop = (e) =>{
+// document.getElementById("screenVideo").src = URL.createObjectURL(
+//   new Blob(data,{
+//     type: data[0].type,
+//   }) 
+// )
+// }
+// }
+// const recordScreen = () =>{
+//   let btn = document.querySelector(".record-btn")
+
+// btn.addEventListener("click", async function () {
+//   let stream = await navigator.mediaDevices.getDisplayMedia({
+//     video: true
+//   })
+
+//   //needed for better browser support
+//   const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9") 
+//              ? "video/webm; codecs=vp9" 
+//              : "video/webm"
+//     let mediaRecorder = new MediaRecorder(stream, {
+//         mimeType: mime
+//     })
+
+//     let chunks = []
+//     mediaRecorder.addEventListener('dataavailable', function(e) {
+//         chunks.push(e.data)
+//     })
+
+//     mediaRecorder.addEventListener('stop', function(){
+//       let blob = new Blob(chunks, {
+//           type: chunks[0].type
+//       })
+//       let url = URL.createObjectURL(blob)
+
+//       let videon = document.querySelector(".videoScreen")
+//       videon.srcObject = url
+
+//       let a = document.createElement('a')
+//       a.href = url
+//       a.download = 'video.webm'
+//       a.click()
+//       window.alert("hello")
+//   })
+
+//     //we have to start the recorder manually
+//     mediaRecorder.start()
+// })
+// }
+
+//audio ke liye
+var startA = function () {
+  navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(stream => {
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+
+    const audioChunks = [];
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
+
+    mediaRecorder.addEventListener("stop", () => {
+      const audioBlob = new Blob(audioChunks);
+    });
+
+    setTimeout(() => {
+      mediaRecorder.stop();
+    }, 9000);
+  });
+};
+
 
 function capture() {
    
-    var video = document.getElementById("video")
+    var video = document.getElementById("video");
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
    let width = canvas.width;
@@ -15,9 +101,10 @@ function capture() {
 
 var save = function (el){
   var canvas = document.getElementById('canvas');
+  
   document.getElementById("el").style.boxShadow = "0";
-var data = canvas.toDataURL('image/jpg');
-el.href=data;
+  var data = canvas.toDataURL('image/jpg');
+  el.href=data;
 
 }
 
@@ -50,3 +137,4 @@ var start = function () {
 $(function () {
   start();
 });
+
